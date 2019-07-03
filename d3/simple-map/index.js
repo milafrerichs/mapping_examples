ee3e7610d992ebd8355e37ae31fbcd2009f8a74e
milafrerichs <mila.frerichs@gmail.com>
2019-07-03 16:12:31 +0200

    Add d3 example

---
+++ d3/simple-map/index.js
@@ -0,0 +1,19 @@
+var url = "https://gist.githubusercontent.com/milafrerichs/78ef5702db2dc514fc2bed465d58406b/raw/f1366ee2a83a9afb1dd2427e9cbd4cd3db8d87ca/bundeslaender_simplify200.geojson";
+d3.json(url).then(function(bb) {
+  var bbox = d3.select('#d3-map').node().getBoundingClientRect();
+  var width = bbox.width;
+  var height = bbox.height;
+  var projection = d3.geoEqualEarth();
+  projection.fitExtent([[20, 20], [width, height]], bb);
+  var geoGenerator = d3.geoPath().projection(projection);
+  var svg = d3.select("#d3-map").append('svg')
+      .style("width", "100%")
+      .style("height", "100%");
+  svg.append('g').selectAll('path')
+    .data(bb.features)
+    .enter()
+    .append('path')
+    .attr('d', geoGenerator)
+    .attr('fill', '#088')
+    .attr('stroke', '#000');
+});
--- mapbox/simple-map/Readme.md
+++ mapbox/simple-map/Readme.md
@@ -1,4 +1,4 @@
 # Mapbox Gl.js Simple Map
 
 [Blog Post](https://mappingwithd3.com/mapbox-d3)
-
+[CodeSandBox](https://codesandbox.io/s/github/milafrerichs/mapping_examples/tree/master/mapbox/simple-map?fontsize=14)
