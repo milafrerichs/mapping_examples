1ed856019a7cb0dbc68434ed2b00175076357578
Mila Frerichs <mila.frerichs@gmail.com>
2019-07-17 12:19:04 +0200

    remove mb

--- mapbox/simple-map/index.js
+++ mapbox/simple-map/index.js
@@ -1,5 +1,5 @@
 var url = "https://gist.githubusercontent.com/milafrerichs/78ef5702db2dc514fc2bed465d58406b/raw/f1366ee2a83a9afb1dd2427e9cbd4cd3db8d87ca/bundeslaender_simplify200.geojson";
-mapboxgl.accessToken = 'pk.eyJ1IjoibWlsYWZyZXJpY2hzIiwiYSI6IkNTOW8tQTgifQ.1Dh_DEF5--fPq_t0mhIkQA';
+mapboxgl.accessToken = 'YOUR MAPBOX TOKEN NEEDS TO BE PLACED HERE';
 var map = new mapboxgl.Map({
   container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
